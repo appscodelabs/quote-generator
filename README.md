fc38b6d81144dbed154508c12010ac4d06666f8e
Tamal Saha <tamal@appscode.com>
2020-12-06 04:44:12 -0800

    added-all

--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 
 - https://developers.google.com/docs/api/quickstart/go
 - https://developers.google.com/drive/api/v3/manage-sharing
+
+
+"stash": "1Y2z7UZIIuvF3Twka6tXoovkbxyxXXz4qLnr9W43BIFs"
+"kubedb-30": "1n8zRoI5qjBaqa5hrogAey8OFd8-q7nCE9ysxwullb0g"
+"kubedb-40": "1s5751cd1SWZAy824njvTz2-iSC4V7NXRoFoCmZfoIcQ"
+"kubedb-45": "1VN3C_fDdUG_-zgFwvPkASVYzVmVr9E2Scv1Z2uqBRrY"
+
+
+{{quote}}
+
+{{name}}
+{{designation}}
+{{company}}
+{{phone}}
+{{email}}
+{{website}}
+
+{{prep-date}}
+{{expiry-date}}
--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"google.golang.org/api/docs/v1"
+	"google.golang.org/api/drive/v3"
 )
+
+var businessFolderId = "1GBmNGzO54HqjlWXrSN7Ds9zu_qJEx4gW"
+
+var docIds = map[string]string{
+	"stash": "1Y2z7UZIIuvF3Twka6tXoovkbxyxXXz4qLnr9W43BIFs",
+	"kubedb-30": "1n8zRoI5qjBaqa5hrogAey8OFd8-q7nCE9ysxwullb0g",
+	"kubedb-40": "1s5751cd1SWZAy824njvTz2-iSC4V7NXRoFoCmZfoIcQ",
+	"kubedb-45": "1VN3C_fDdUG_-zgFwvPkASVYzVmVr9E2Scv1Z2uqBRrY",
+}
 
 // Retrieves a token, saves the token, then returns the generated client.
 func getClient(config *oauth2.Config) *http.Client {
@@ -78,15 +92,34 @@ func main() {
 		"https://www.googleapis.com/auth/documents",
 		"https://www.googleapis.com/auth/documents.readonly",
 		"https://www.googleapis.com/auth/drive",
+		"https://www.googleapis.com/auth/drive.appdata",
 		"https://www.googleapis.com/auth/drive.file",
+		"https://www.googleapis.com/auth/drive.metadata",
+		"https://www.googleapis.com/auth/drive.metadata.readonly",
+		"https://www.googleapis.com/auth/drive.photos.readonly",
 		"https://www.googleapis.com/auth/drive.readonly",
+		"https://www.googleapis.com/auth/drive.scripts",
 	)
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
 	client := getClient(config)
 
-	srv, err := docs.NewService(context.TODO(), option.WithHTTPClient(client), option.WithScopes(
+	srvDrive, err := drive.NewService(context.TODO(), option.WithHTTPClient(client), option.WithScopes(
+		"https://www.googleapis.com/auth/drive",
+		"https://www.googleapis.com/auth/drive.appdata",
+		"https://www.googleapis.com/auth/drive.file",
+		"https://www.googleapis.com/auth/drive.metadata",
+		"https://www.googleapis.com/auth/drive.metadata.readonly",
+		"https://www.googleapis.com/auth/drive.photos.readonly",
+		"https://www.googleapis.com/auth/drive.readonly",
+		"https://www.googleapis.com/auth/drive.scripts",
+	))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Docs client: %v", err)
+	}
+
+	srvDoc, err := docs.NewService(context.TODO(), option.WithHTTPClient(client), option.WithScopes(
 		"https://www.googleapis.com/auth/documents",
 		"https://www.googleapis.com/auth/documents.readonly",
 		"https://www.googleapis.com/auth/drive",
@@ -100,26 +133,140 @@ func main() {
 	// Prints the title of the requested doc:
 	// https://docs.google.com/document/d/195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE/edit
 	docId := "1sVzl3bKYDqdCaefh9lP6hlqgkhX7LNgCCMVl80EwHIk"
-	doc, err := srv.Documents.Get(docId).Do()
+	doc, err := srvDoc.Documents.Get(docId).Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve data from document: %v", err)
 	}
 	fmt.Printf("The title of the doc is: %s\n", doc.Title)
 
-	err = run(srv)
+	err = run(srvDoc, srvDrive)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func run(srv *docs.Service) error {
-	doc := &docs.Document{}
-	doc.Title = "XYZ"
-	doc, err := srv.Documents.Create(doc).Do()
+type Info struct {
+	Quote string
+
+	Name        string
+	Designation string
+	Company     string
+	Phone       string
+	Email       string
+
+	PrepDate   time.Time
+	ExpiryDate time.Time
+}
+
+func (i Info) Date() map[string]string {
+	data := map[string]string{}
+
+	data["{{quote}}"] = i.Quote
+	data["{{name}}"] = i.Name
+	data["{{designation}}"] = i.Designation
+	data["{{company}}"] = i.Company
+	data["{{phone}}"] = i.Phone
+	data["{{email}}"] = i.Email
+	data["{{website}}"] = Domain(i.Email)
+	data["{{prep-date}}"] = i.PrepDate.Format("Jan 2, 2006")
+	data["{{expiry-date}}"] = i.ExpiryDate.Format("Jan 2, 2006")
+	return data
+}
+
+func Domain(email string) string {
+	parts := strings.Split(email, "@")
+	return parts[len(parts)-1]
+}
+
+func run(srvDoc *docs.Service, srvDrive *drive.Service) error {
+	info := Info{
+		Quote:       "AC2012001",
+		Name:        "Tamal Saha",
+		Designation: "CEO",
+		Company:     "AppsCode Inc.",
+		Phone:       "+1(434)284-0668",
+		Email:       "tamal@appscode.com",
+		PrepDate:    time.Now(),
+		ExpiryDate:  time.Now().Add(30 * 24 * time.Hour),
+	}
+
+	// https://developers.google.com/drive/api/v3/folder#java
+	folderMetadata := &drive.File{
+		Name:     Domain(info.Email),
+		MimeType: "application/vnd.google-apps.folder",
+		Parents:  []string{businessFolderId},
+	}
+	folder, err := srvDrive.Files.Create(folderMetadata).Fields("id").Do()
 	if err != nil {
 		return err
 	}
-	fmt.Println(">>>>>>>>>>>>>", doc.Title)
+	fmt.Println(folder.Id)
+
+	// https://developers.google.com/docs/api/how-tos/documents#copying_an_existing_document
+	copyMetadata := &drive.File{
+		Name:    fmt.Sprintf("%s QUOTE #%s", info.Company, info.Quote),
+		Parents: []string{folder.Id},
+	}
+	copyFile, err := srvDrive.Files.Copy(docIds["stash"], copyMetadata).Fields("id", "parents").Do()
+	if err != nil {
+		return err
+	}
+	fmt.Println(copyFile.Id)
+
+	// https://developers.google.com/docs/api/how-tos/merge
+	fields := info.Date()
+	req := &docs.BatchUpdateDocumentRequest{
+		Requests: make([]*docs.Request, 0, len(fields)),
+	}
+	for k, v := range fields {
+		req.Requests = append(req.Requests, &docs.Request{
+			ReplaceAllText: &docs.ReplaceAllTextRequest{
+				ContainsText: &docs.SubstringMatchCriteria{
+					MatchCase: true,
+					Text:      k,
+				},
+				ReplaceText: v,
+			},
+		})
+	}
+	doc, err := srvDoc.Documents.BatchUpdate(copyFile.Id, req).Do()
+	if err != nil {
+		return err
+	}
+
+	//doc := &docs.Document{}
+	//doc.Title = "XYZ"
+	//doc, err := srvDoc.Documents.Create(doc).Do()
+	//if err != nil {
+	//	return err
+	//}
+
+	/*
+		{{quote}}
+
+		{{name}}
+		{{designation}}
+		{{company}}
+		{{phone}}
+		{{email}}
+		{{website}}
+
+		{{prep-date}}
+		{{expiry-date}}
+	*/
+
 	fmt.Println(">>>>>>>>>>>>>", doc.DocumentId)
+
+	resp, err := srvDrive.Files.Export(doc.DocumentId, "application/pdf").Download()
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	var buf bytes.Buffer
+	io.Copy(&buf, resp.Body)
+	err = ioutil.WriteFile("/home/tamal/Downloads/1a/test-quote.pdf", buf.Bytes(), 0644)
+	if err != nil {
+		return err
+	}
 	return nil
 }
